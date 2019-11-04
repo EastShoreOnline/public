e5a203805a82cda4dd1ce1a46bf9bb2fea21a3ef
jordan-bender <jbender@alpinedistrict.org>
2019-11-04 14:27:57 -0700

    Update sl_triggers.js

--- sl_triggers.js
+++ sl_triggers.js
@@ -1,5 +1,4 @@
 window.setTimeout(function(){
-    console.log(document.getElementById("triggers"),window.parent.document);
     var trigger_element = document.getElementById("triggers")
     var trigger_times = trigger_element.getAttribute("data-times")
     var trigger_vars = trigger_element.getAttribute("data-vars")
