2f9b1b3eea184939a4bb3f15459cbceb4b7ba7fa
thedarci <drhoades@alpinedistrict.org>
2019-11-06 21:26:12 -0700

    Update sl_triggers.js

--- sl_triggers.js
+++ sl_triggers.js
@@ -1,17 +1,25 @@
 window.setTimeout(function(){
-    alert('yo');
     var trigger_element = document.getElementById("triggers")
-    var trigger_times = trigger_element.getAttribute("data-times")
-    var trigger_vars = trigger_element.getAttribute("data-vars")
-    var my_video = document.getElementById("my_video")
-    
-    my_video.addEventListener("timeupdate", function(){
-    if(this.currentTime >= trigger_times*1) {
-        this.pause();
-        console.log(trigger_vars)
-       parent.parent.postMessage(trigger_vars,'*')
+    var trigger_times = trigger_element.getAttribute("data-times").split(',');
+    var trigger_vars = trigger_element.getAttribute("data-vars").split(',');
+    if(trigger_times!=trigger_vars){
+     alert("The number of video triggers does not match the number of variables.")   
     }
-        
-});
+    var my_video = document.getElementById("my_video")
+    var trigger_array = [];
+    var this_time;
+    var this_var;
+    for(var a=0; a<trigger_times.length; a++){
+        trigger_array[a]=0;
+    }      
+    my_video.addEventListener("timeupdate", function(){
+        for(a=0; a<trigger_times.length; a++){
+            if(this.currentTime >= trigger_times[a]*1 && trigger_array[a]==0) {
+                this.pause();
+                parent.postMessage(this_var,'*');
+                trigger_array[a]==1;
+            }
+        }
+    });
 
 },1000);
