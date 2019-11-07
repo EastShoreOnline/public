cfa66cab466d4491faee2018eb5215a20e98580d
thedarci <drhoades@alpinedistrict.org>
2019-11-06 21:49:07 -0700

    Update sl_triggers.js

--- sl_triggers.js
+++ sl_triggers.js
@@ -4,7 +4,7 @@ window.setTimeout(function(){
     console.log(trigger_element.getAttribute("data-times"));
     var trigger_times = ''+trigger_element.getAttribute("data-times").split(',');
     var trigger_vars = ''+trigger_element.getAttribute("data-vars").split(',');
-    if(trigger_times!=trigger_vars){
+    if(trigger_times.length!=trigger_vars.length){
      alert("The number of video triggers does not match the number of variables.")   
     }
     var my_video = document.getElementById("my_video")
