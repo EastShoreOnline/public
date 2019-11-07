5821a8e5cf9334841aceebfd5b4b01ff16b901ce
thedarci <drhoades@alpinedistrict.org>
2019-11-06 21:54:23 -0700

    Update sl_triggers.js

--- sl_triggers.js
+++ sl_triggers.js
@@ -4,6 +4,7 @@ window.setTimeout(function(){
     console.log(trigger_element.getAttribute("data-times"));
     var trigger_times = ''+trigger_element.getAttribute("data-times").split(',');
     var trigger_vars = ''+trigger_element.getAttribute("data-vars").split(',');
+    console.log(trigger_times,trigger_vars);
     if(trigger_times.length!=trigger_vars.length){
      alert("The number of video triggers does not match the number of variables.")   
     }
@@ -20,7 +21,7 @@ window.setTimeout(function(){
             if(this.currentTime >= trigger_times[a]*1 && trigger_array[a]==0) {
                 this.pause();
                 parent.postMessage(this_var,'*');
-                trigger_array[a]==1;
+                trigger_array[a]=1;
             }
         }
     });
