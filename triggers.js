493ba1c096f65246c3ef20b6859cf343256c12aa
thedarci <drhoades@alpinedistrict.org>
2019-11-13 12:44:09 -0700

    Update triggers.js

--- triggers.js
+++ triggers.js
@@ -1,41 +1,37 @@
+//array to hold multiple trigger times
+var trigger_times=[];
+var trigger_vars=[];
 window.setTimeout(function(){
-    var trigger_element = document.getElementById("triggers");
-    
-    console.log(trigger_element.getAttribute("data-times"));
-    var trigger_times = ''+trigger_element.getAttribute("data-times");
-    var trigger_vars = ''+trigger_element.getAttribute("data-vars");
-    if(trigger_times.indexOf(',')==-1){
-        trigger_times=[trigger_times];   
-    }
-    else{
-       trigger_times=trigger_times.split(',');   
-    }
-    if(trigger_vars.indexOf(',')==-1){
-        trigger_vars=[trigger_vars];   
-    }
-    else{
-        trigger_vars=trigger_vars.split(',');   
-    }
-    console.log(trigger_times,trigger_vars);
-    if(trigger_times.length!=trigger_vars.length){
-     alert("The number of video triggers does not match the number of variables.")   
-    }
-    var my_video = document.getElementById("my_video")
-    var trigger_array = [];
-    var this_time;
-    var this_var;
-    for(var a=0; a<trigger_times.length; a++){
-        trigger_array[a]=0;
-    }      
-    my_video.addEventListener("timeupdate", function(){
-        for(a=0; a<trigger_times.length; a++){
-            console.log(this.currentTime,trigger_times[a]*1,trigger_array[a]);
-            if(this.currentTime >= trigger_times[a]*1 && trigger_array[a]==0) {
-                this.pause();
-                parent.postMessage(trigger_vars[a],'*');
-                trigger_array[a]=1;
-            }
+    if(video_triggers!=''){
+        if(video_triggers.indexOf(',')==-1){
+            trigger_times=[video_triggers];   
         }
-    });
-
+        else{
+           trigger_times=video_triggers.split(',');   
+        }
+        if(video_variables.indexOf(',')==-1){
+            trigger_vars=[video_variables];   
+        }
+        else{
+            trigger_vars=video_variables.split(',');   
+        }
+        if(trigger_times.length!=trigger_vars.length){
+         alert("The number of video triggers does not match the number of variables.")   
+        }
+        var trigger_array = [];
+        var this_time;
+        var this_var;
+        for(var a=0; a<trigger_times.length; a++){
+            trigger_array[a]=0;
+        }      
+        my_video.addEventListener("timeupdate", function(){
+            for(a=0; a<trigger_times.length; a++){
+                if(this.currentTime >= trigger_times[a]*1 && trigger_array[a]==0) {
+                    this.pause();
+                    parent.postMessage(trigger_vars[a],'*');
+                    trigger_array[a]=trigger_array[a]+1;
+                }
+            }
+        });
+    }
 },1000);
